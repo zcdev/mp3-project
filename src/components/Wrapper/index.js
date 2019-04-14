847aa50898a434064d03d4d7ed19f5d36fb847c8
ZCDEV <zcdev@Orange.attlocal.net>
2019-04-14 01:17:50 -0700

    credits

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -5,7 +5,7 @@ function Wrapper(props) {
   return <div><main className="wrapper">{props.children}</main>
   <footer><p>&copy; Zoe Chang. All rights reserved (Library of Congress).</p>
   <p>All songs written, composed, arranged, performed, recorded, and produced by Zoe Chang</p>
-  <p>Special thanks: guitars &amp; drums by Agostia @Alone</p></footer></div>;
+  <p>Special thanks: guitars, bass, and drums by Agostia @Alone</p></footer></div>;
 }
 
 export default Wrapper;
