d2be5b451af7555207946a0b5c53251a10df87ff
ZCDEV <zcdev@Orange.attlocal.net>
2019-04-14 00:46:54 -0700

    spinning button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import SongFile from "./components/SongFile";
+
 import Wrapper from "./components/Wrapper";
 import Songs from "./Playlist.json";
 const music = new Audio('songs/fly.mp3','songs/closer.mp3', 'songs/it-will-never-end.mp3', 'songs/alone.mp3','songs/open-door', 'music/hello.mp3');
-
+let spin = "";
 class App extends Component {
 
   state = {
@@ -13,7 +13,7 @@ class App extends Component {
 
   playSong = id => {
 
-  music.src = Songs[id-1].song;
+  music.src = Songs[id].song;
 
     if (this.state.Clicked.includes(id)) {
 
@@ -21,29 +21,48 @@ class App extends Component {
         Clicked: []
       });
 
-      music.pause();
-
     } else {
       
       this.setState({
         Clicked: this.state.Clicked.concat(id)
       });
 
-      music.play();
+      if (this.state.Clicked[0] !== undefined) {
+        music.pause();
+      } else {
+        music.play();
+      }
     }
+    
+  };
+
+  clickedButton = id => {
+
+    if (!spin.length) {
+      if (this.state.Clicked[0] === Songs[id].id) {
+          return spin = "spin";
+      }
+      if (!this.state.Clicked[0]=== undefined){
+        return spin = "spin"; 
+      }
+    } else {
+      if (this.state.Clicked[0]=== undefined){
+        return spin = "";  
+      }
+    }
+   
   };
 
   render() {
     return (
       <Wrapper>
-        {this.state.Songs.map(song => (
-          <SongFile
-            playSong={this.playSong}
-            id={song.id}
-            key={song.id}
-            image={song.image}
-            title={song.title}
-          />
+        {this.state.Songs.map((song, id) => (
+          <div className="card" onClick={() => this.playSong(id)} id={song.id} key={song.id}>
+            <div className="container">
+              <img className="photo" src={song.image} alt={song.title} title={song.title} />
+              <div className="button"><img className={this.clickedButton(id)} src="./images/play-pause.png" alt="play/pause"/><span>{song.title}</span></div>
+            </div>
+        </div>
         ))}
       </Wrapper>
     );
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Wrapper(props) {
   return <div><main className="wrapper">{props.children}</main>
   <footer><p>&copy; Zoe Chang. All rights reserved (Library of Congress).</p>
-  <p>All songs written, composed, arranged, performed, and recorded by Zoe Chang</p>
+  <p>All songs written, composed, arranged, performed, recorded, and produced by Zoe Chang</p>
   <p>Special thanks: guitars &amp; drums by Agostia @Alone</p></footer></div>;
 }
 
