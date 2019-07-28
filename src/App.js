e87982d4a0a452554e1546d836204750a82c5187
ZCDEV <zcdev@Orange.attlocal.net>
2019-07-28 01:51:16 -0700

    Refactor all code and now spinning stops

--- src/App.js
+++ src/App.js
@@ -3,54 +3,45 @@ import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Songs from "./Playlist.json";
 const music = new Audio('songs/fly.mp3','songs/closer.mp3', 'songs/it-will-never-end.mp3', 'songs/alone.mp3','songs/open-door', 'music/hello.mp3');
-let spin = "";
+let currentTime;
+let duration;
+
+
 class App extends Component {
 
   state = {
     Songs,
-    Clicked: []
+    selected: {}
   };
 
   playSong = id => {
 
-  music.src = Songs[id].song;
+    let selected = this.state.selected;
+    selected[id] = !selected[id];
+    this.setState({selected: selected});
+    music.src = Songs[id].song;
 
-    if (this.state.Clicked.includes(id)) {
+    music.onloadedmetadata = () => {
+      duration = music.duration.toFixed(2);
+    };
 
-      this.setState({
-        Clicked: []
-      });
-
-    } else {
-      
-      this.setState({
-        Clicked: this.state.Clicked.concat(id)
-      });
-
-      if (this.state.Clicked[0] !== undefined) {
-        music.pause();
-      } else {
-        music.play();
-      }
+    let getSongLength = () => {
+      currentTime = music.currentTime.toFixed(2);
+      // console.log("duration: " + duration);
+      // console.log("currentTime: " + currentTime);
+      if (currentTime === duration) {
+        this.setState({selected: !selected});
+      } 
     }
-    
-  };
-
-  clickedButton = id => {
-
-    if (!spin.length) {
-      if (this.state.Clicked[0] === Songs[id].id) {
-          return spin = "spin";
-      }
-      if (!this.state.Clicked[0]=== undefined){
-        return spin = "spin"; 
-      }
+ 
+    music.addEventListener("timeupdate", getSongLength);
+ 
+    if (this.state.selected[id]) {
+      music.play()
     } else {
-      if (this.state.Clicked[0]=== undefined){
-        return spin = "";  
-      }
+      music.pause();     
     }
-   
+
   };
 
   render() {
@@ -60,7 +51,7 @@ class App extends Component {
           <div className="card" onClick={() => this.playSong(id)} id={song.id} key={song.id}>
             <div className="container">
               <img className="photo" src={song.image} alt={song.title} title={song.title} />
-              <div className="button"><img className={this.clickedButton(id)} src="./images/play-pause.png" alt="play/pause"/><span>{song.title}</span></div>
+              <div className="button"><img className={this.state.selected[id] ? "spin": ""} src="./images/play-pause.png" alt="play/pause"/><span>{song.title}</span></div>
             </div>
         </div>
         ))}
