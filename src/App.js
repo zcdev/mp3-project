d10ca3193f10477cb169ee0feb9955b5043238fe
ZCDEV <zoechangdev@gmail.com>
2020-03-31 07:08:23 -0700

    Add preload for songs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Songs from "./Playlist.json";
 const music = new Audio('songs/fly.mp3', 'songs/closer.mp3', 'songs/it-will-never-end.mp3', 'songs/alone.mp3', 'songs/open-door', 'music/hello.mp3');
+music.preload = 'auto';
 class App extends Component {
 
     constructor() {
@@ -30,7 +31,7 @@ class App extends Component {
     playSong = id => {
         let currentTime = music.currentTime.toFixed(2);
         music.src = Songs[id].song;
-        
+
         music.addEventListener(
             "timeupdate", () => {
                 this.setState({
